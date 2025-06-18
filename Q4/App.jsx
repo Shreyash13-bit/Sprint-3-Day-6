7397d86f168b158e9f6037d3c7f39f7b442ebe1d
Shreyash Roy <22052762@kiit.ac.in>
2025-06-18 07:47:04 +0530

    Create App.jsx

---
+++ Q4/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './ProductList';
+import ProductDetail from './ProductDetail';
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<ProductList />} />
+      <Route path="/products/:id" element={<ProductDetail />} />
+    </Routes>
+  </Router>
+);
+export default App;
